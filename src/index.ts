864905206060a5f57db74f93bd6839dcb0fa7219
Rafael <rafaelmoura7033.com>
2024-06-15 09:08:08 -0300

    Using TV Class

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello world!");
+import TV from "./TV";
+
+const tv = new TV("Globo", 5, true); // Instanciando a classe TV
+tv.statusTV(); // Status atual da TV
+
+tv.aumentarVolume();
+tv.aumentarVolume();
+tv.diminuirVolume();
+tv.volumeAtual(); // Volume depois de mexer nele três vezes
+
+tv.canalAtual();
+tv.mudarCanal("SBT"); // Mudando canal
+tv.canalAtual();
+
+tv.statusTV();
+tv.ligarOuDesligarTV();
+tv.statusTV;
